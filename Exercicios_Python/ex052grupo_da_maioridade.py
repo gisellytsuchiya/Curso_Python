cd17fdb432adfeaf8a7d31fd2ca9e80de7457b35
Giselly Tsuchiya <yumi.giselly@gmail.com>
2023-01-08 16:36:36 -0400

    exercicios novos
     correção de exercicios de python do exercicio 45 ao 54 .

--- Exercicios_Python/ex052grupo_da_maioridade.py
+++ Exercicios_Python/ex052grupo_da_maioridade.py
@@ -1,2 +1,16 @@
 #Crie um programa que leia o ano de nascimento de sete pessoas . No final , mostre
 #quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
+import datetime
+atual = datetime.date.today().year
+soma = 0
+cont = 0
+for ano in range (7):
+    pessoas = int(input("Digite o ano que você nasceu :"))
+    idade = atual - pessoas 
+    if idade >= 18:
+        soma = soma + 1
+    else:
+        cont = cont + 1 
+print("Apenas {} pessoas atingiram a maior idade.".format(soma))
+print("E somente {} pessoas não atingiram a maior idade.".format(cont))
